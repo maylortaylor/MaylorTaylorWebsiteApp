ba4b84d2e0c4052601c14fe46c39ab49cb19e8e5
maylortaylor <maylortaylor@gmail.com>
2019-02-26 17:47:06 -0500

    removed old project

--- maylortaylorwebsite/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:maylortaylorwebsite/app/appRoot.dart';
-
-void main() {
-  debugPaintSizeEnabled = false;
-  runApp(AppRootWidget());
-}
